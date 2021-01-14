ec056e66195b4dac09e4e1d1a9f2b68bc4d583bd
muraddagha <muradha@code.edu.az>
2021-01-14 13:29:58 +0400

    Menu fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let menuBtn = document.querySelector(".navbar-toggle");
 menuBtn.addEventListener("click", () => {
   document.querySelector(".toggle-menu").classList.toggle("active");
   document.querySelector("body").classList.toggle("modal-openn");
+  document.querySelector("html").classList.toggle("modal-openn");
   menuBtn.parentNode.classList.toggle("container");
   menuBtn.parentNode.classList.toggle("container-fluid");
 });

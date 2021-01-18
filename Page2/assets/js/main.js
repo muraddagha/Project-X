440ee7f2a6304ceaefb4df61e24ef9f84fca5a5c
muraddagha <muradha@code.edu.az>
2021-01-18 22:38:40 +0400

    Update

--- Page2/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-let menuBtn = document.querySelector(".navbar-toggle");
-
-menuBtn.addEventListener("click", () => {
-  document.querySelector(".toggle-menu").classList.toggle("active");
-  document.querySelector("body").classList.toggle("block-scroll");
-  document.querySelector("html").classList.toggle("block-scroll");
-  menuBtn.parentNode.classList.toggle("container");
-  menuBtn.parentNode.classList.toggle("container-fluid");
-});
